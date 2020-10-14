fec235e7d149baf572163c616d6cbfb21f0ef6ec
Anderson Rodrigues <anddersonrds@gmail.com>
2020-10-14 19:17:27 -0300

    add new routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,16 @@ import { Route, Switch } from 'react-router-dom';
 
 import Landing from '../pages/Landing';
 import OrphanagesMap from '../pages/OrphanagesMap';
+import Orphanage from '../pages/Orphanage';
+import OrphanageCreate from '../pages/OrphanageCreate';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Landing} />
     <Route path="/app" exact component={OrphanagesMap} />
+
+    <Route path="/orphanages/:id" exact component={Orphanage} />
+    <Route path="/orphanages/create" exact component={OrphanageCreate} />
   </Switch>
 );
 
